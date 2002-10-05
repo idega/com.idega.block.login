c069a907cd6b2d25503a639cd3cc3198666d6583
Aron Birkir Gudmundsson <aron@idega.com>
2002-10-05 03:11:40 +0000

    *** empty log message ***

---
+++ src/java/com/idega/block/login/business/LoginGroupPageListener.java
@@ -0,0 +1,81 @@
+package com.idega.block.login.business;
+
+import com.idega.business.*;
+import com.idega.presentation.IWContext;
+import com.idega.idegaweb.IWException;
+import com.idega.user.data.Group;
+import com.idega.builder.business.BuilderLogic;
+import java.util.*;
+
+/**
+ * @author aron
+ *
+ */
+public class LoginGroupPageListener implements IWEventListener {
+	
+	private static String prmGroupToPageMap = "login_group_to_page_map";
+	
+	public boolean actionPerformed(IWContext iwc)throws IWException{
+		/** todo: */
+		if ( iwc.isLoggedOn() && LoginBusiness.isLogOnAction(iwc) ){
+			System.err.println("trying to get page for usergroup");
+			String page = checkUserGroups(iwc);
+			if(page!=null){
+				System.err.println("setting group page");
+				BuilderLogic.getInstance().setCurrentPriorityPageID(iwc,page);
+			}
+		}
+		return false;
+	}
+	
+	private String checkUserGroups(IWContext iwc){
+		List userGroups = LoginBusiness.getPermissionGroups(iwc);
+		Map G2P = getLoginGroupPageMap(iwc);
+		if(userGroups!=null ){
+			if(G2P !=null){
+				Iterator iter = userGroups.iterator();
+				Group g;
+				String gid;
+				while(iter.hasNext()){
+					g = (Group) iter.next();
+					try{
+					gid = g.getPrimaryKey().toString();
+					if(G2P.containsKey(gid))
+						return (String) G2P.get(gid);
+					}
+					catch(java.rmi.RemoteException ex){
+					
+					}
+				}
+			}
+			else{
+			System.err.println("no pagesInMemory ");
+			}
+		}
+		else{
+			System.err.println("no userGroups ");
+		}
+		return null;
+	}
+	
+	public static void setLoginGroupPageMap(IWContext iwc,Map GroupToPageMap ){
+		iwc.setApplicationAttribute(prmGroupToPageMap,GroupToPageMap);
+	}
+	
+	public static Map getLoginGroupPageMap(IWContext iwc){
+		Object map = iwc.getApplicationAttribute(prmGroupToPageMap);
+		if(map!=null)
+			return (Map) map;
+		else
+			map = new HashMap();
+		return null;
+	}
+	
+	public static void addGroupPageMapping(IWContext iwc,String groupId,String pageId){
+		Map m = getLoginGroupPageMap(iwc);
+		if(m.containsKey(groupId)){
+			m.put(groupId,pageId);
+		}
+		setLoginGroupPageMap(iwc,m);
+	}
+}
