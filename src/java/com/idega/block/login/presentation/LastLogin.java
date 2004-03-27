e156efe0115db61bf97f3a0c2e41a5f871bb2cdb
Aron Birkir Gudmundsson <aron@idega.com>
2004-03-27 22:53:19 +0000

    Login info blocks added, LastLogin and OnlineUsers

---
+++ src/java/com/idega/block/login/presentation/LastLogin.java
@@ -0,0 +1,59 @@
+/*
+ * Created on Mar 27, 2004
+ *
+ */
+package com.idega.block.login.presentation;
+
+import java.rmi.RemoteException;
+import java.sql.Date;
+import java.text.DateFormat;
+
+import com.idega.block.login.business.LoginBusiness;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.text.Text;
+
+/**
+ * LastLogin
+ * @author aron 
+ * @version 1.0
+ */
+public class LastLogin extends Block {
+	
+	private String style = "";
+	
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#main(com.idega.presentation.IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+		if(iwc.isLoggedOn()){
+			try {
+				IWResourceBundle iwrb = getResourceBundle(iwc);
+				String text = iwrb.getLocalizedString("last_login_text","Last login");
+				DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT,DateFormat.SHORT,iwc.getCurrentLocale());
+				Date last = LoginBusiness.getLastLoginByUser(new Integer(iwc.getUserId()));
+				Text txt = new Text(text+" "+df.format(last));
+				txt.setStyleAttribute(this.style);
+				add(txt);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#getBundleIdentifier()
+	 */
+	public String getBundleIdentifier() {
+		return Login.IW_BUNDLE_IDENTIFIER;
+	}
+	/**
+	 * @param style The style to set.
+	 */
+	public void setStyle(String style) {
+		this.style = style;
+	}
+}
---
+++ src/java/com/idega/block/login/presentation/OnlineUsers.java
@@ -0,0 +1,73 @@
+/*
+ * Created on Mar 27, 2004
+ *
+ */
+package com.idega.block.login.presentation;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.idega.core.accesscontrol.business.LoggedOnInfo;
+import com.idega.core.accesscontrol.business.LoginBusinessBean;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+
+/**
+ * OnlineUsers
+ * @author aron 
+ * @version 1.0
+ */
+public class OnlineUsers extends Block {
+	
+	private boolean showNobodyText = true;
+	private boolean showPersonalID = false;
+	private boolean showLoginName = false;
+	
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#main(com.idega.presentation.IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+		add(getLoggedInUsers(iwc));
+	}
+	
+	
+	
+	
+	public PresentationObject getLoggedInUsers(IWContext iwc){
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		Table table = new Table();
+		int row = 1;
+		Text tUsers = new Text(iwrb.getLocalizedString("online_users","Online users:"));
+		tUsers.setBold();
+		table.add(tUsers,1,row++);
+		Collection usersLoggedIn = LoginBusinessBean.getLoggedOnInfoList(iwc);
+		if(usersLoggedIn!=null && !usersLoggedIn.isEmpty()){
+			for (Iterator iter = usersLoggedIn.iterator(); iter.hasNext();) {
+				int col = 1;
+				LoggedOnInfo info = (LoggedOnInfo) iter.next();
+				table.addText(info.getUser().getName(),col++,row++);
+				if(showPersonalID)
+					table.addText(info.getUser().getPersonalID(),col++,row++);
+				if(showLoginName)
+					table.addText(info.getLogin(),col++,row++);
+			}
+		}
+		else{
+			Text tNone = new Text(iwrb.getLocalizedString("nobody_loggedon","nobody"));
+			table.add(tNone,1,row);
+		}
+		table.mergeCells(1,1,table.getColumns(),1);
+		
+		return table;
+	}
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#getBundleIdentifier()
+	 */
+	public String getBundleIdentifier() {
+		return Login.IW_BUNDLE_IDENTIFIER;
+	}
+}
