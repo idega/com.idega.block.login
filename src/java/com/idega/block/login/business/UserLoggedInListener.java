e3e5ecd9a83a2703f9b193a01993459f839fc2c5
Vitautas Civilis <civilis@idega.com>
2009-01-22 12:40:33 +0000

    formatted

--- src/java/com/idega/block/login/business/UserLoggedInListener.java
+++ src/java/com/idega/block/login/business/UserLoggedInListener.java
@@ -8,24 +8,21 @@ import com.idega.util.expression.ELUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
- *
- * Last modified: $Date: 2008/07/02 19:24:29 $ by $Author: civilis $
- *
+ * @version $Revision: 1.3 $ Last modified: $Date: 2009/01/22 12:40:33 $ by $Author: civilis $
  */
 public class UserLoggedInListener implements AuthenticationListener {
-
+	
 	public String getAuthenticationListenerName() {
 		return "login.UserLoggedInListener";
 	}
-
-	public void onLogoff(IWContext iwc, User lastUser)
-			throws ServletFilterChainInterruptException {
-	}
-
-	public void onLogon(IWContext iwc, User loggedInUser)
-			throws ServletFilterChainInterruptException {
 	
+	public void onLogoff(IWContext iwc, User lastUser)
+	        throws ServletFilterChainInterruptException {
+	}
+	
+	public void onLogon(IWContext iwc, User loggedInUser)
+	        throws ServletFilterChainInterruptException {
+		
 		UserLoggedInEvent ev = new UserLoggedInEvent(loggedInUser);
 		ev.setIWC(iwc);
 		
