2c79171cbf8630c240b04e28fd7f26a6e6ba0f47
Valdas Žemaitis <valdas@idega.com>
2012-07-24 14:28:08 +0300

    Added link type for fancy box; removed warning

--- src/java/com/idega/block/login/presentation/UserPasswordChanger.java
+++ src/java/com/idega/block/login/presentation/UserPasswordChanger.java
@@ -15,34 +15,33 @@ import com.idega.util.CoreConstants;
 
 /**
  *
- * 
- * @author <a href="anton@idega.com">Anton Makarov</a>
- * @version Revision: 1.0 
  *
- * Last modified: Oct 1, 2008 by Author: Anton 
+ * @author <a href="anton@idega.com">Anton Makarov</a>
+ * @version Revision: 1.0
+ *
+ * Last modified: Oct 1, 2008 by Author: Anton
  *
  */
 
 public class UserPasswordChanger extends Block {
 
-	private static final String HEADER_ID = "header";	
+	private static final String HEADER_ID = "header";
 	private static final String ERROR_MESSAGE_ID = "message";
 	private static final String PASSWORD_CHANGER_STYLE = "passwordChanger";
-	
+
 	private IWBundle bundle = null;
 	private IWResourceBundle iwrb = null;
-	
+
 	@Override
 	public String getBundleIdentifier() {
 		return Login2.IW_BUNDLE_IDENTIFIER;
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	private void initializeLocalVariables(IWContext iwc) {
 		bundle = getBundle(iwc);
 		iwrb = bundle.getResourceBundle(iwc);
 	}
-	
+
 	@Override
 	public void main(IWContext iwc) throws IOException {
 		initializeLocalVariables(iwc);
@@ -50,9 +49,9 @@ public class UserPasswordChanger extends Block {
 		container.setID(PASSWORD_CHANGER_STYLE);
 		container.setStyleClass("passwords");
 		add(container);
-		
+
 		String styleName = "webfaceFormItem";
-		
+
 		// Header
 		Layer headerContainer = new Layer();
 		container.add(headerContainer);
@@ -60,7 +59,7 @@ public class UserPasswordChanger extends Block {
 		Text header = new Text(iwrb.getLocalizedString("change_your_password", "Please change your password"));
 		header.setID(HEADER_ID);
 		headerContainer.add(header);
-		
+
 		// Error message
 		Layer messageContainer = new Layer();
 		container.add(messageContainer);
@@ -68,7 +67,7 @@ public class UserPasswordChanger extends Block {
 		Text msgOutput = new Text("");
 		msgOutput.setID(ERROR_MESSAGE_ID);
 		messageContainer.add(msgOutput);
-		
+
 		// User password
 		Layer passwordContainer = new Layer();
 		container.add(passwordContainer);
@@ -79,7 +78,7 @@ public class UserPasswordChanger extends Block {
 		Label passwordLabel = new Label(iwrb.getLocalizedString("new_password", "New password"), passwordInput);
 		passwordContainer.add(passwordLabel);
 		passwordContainer.add(passwordInput);
-		
+
 		// User password verify
 		Layer password2Container = new Layer();
 		container.add(password2Container);
@@ -89,21 +88,21 @@ public class UserPasswordChanger extends Block {
 
 		Label password2Label = new Label(iwrb.getLocalizedString("verify_password", "Verify password"), password2Input);
 		password2Container.add(password2Label);
-		password2Container.add(password2Input);		
-		
+		password2Container.add(password2Input);
+
 		// Save button
 		Layer buttonsContainer = new Layer();
 		container.add(buttonsContainer);
 		buttonsContainer.setStyleClass("webfaceButtonLayer");
-		
+
 		GenericButton saveButton = new GenericButton(iwrb.getLocalizedString("save", "Save"));
-		
+
 		StringBuffer onClickScript = new StringBuffer("validateAndSavePassword('").append(ERROR_MESSAGE_ID)
 		.append(CoreConstants.QOUTE_SINGLE_MARK).append(CoreConstants.COMMA).append(CoreConstants.QOUTE_SINGLE_MARK)
 		.append(getResourceBundle(iwc).getLocalizedString("password_validation_error", "Passwords do not match")).append(CoreConstants.QOUTE_SINGLE_MARK)
 		.append(CoreConstants.COMMA).append(CoreConstants.QOUTE_SINGLE_MARK).append(getResourceBundle(iwc).getLocalizedString("password_save_error", "System error. Password was not saved"))
 		.append(CoreConstants.QOUTE_SINGLE_MARK).append(");");
-			
+
 		saveButton.setOnClick(onClickScript.toString());
 		buttonsContainer.add(saveButton);
 	}
