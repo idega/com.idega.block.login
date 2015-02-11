994ed0ea70b826ad710319afca9d34575c4beafd
Valdas <valdas@idega.com>
2015-02-11 16:25:11 +0200

    Improvements

--- src/java/com/idega/block/login/servlet/filter/LoginAuthorizationFilter.java
+++ src/java/com/idega/block/login/servlet/filter/LoginAuthorizationFilter.java
@@ -19,43 +19,45 @@ import com.idega.util.RequestUtil;
 import com.idega.util.StringUtil;
 import com.idega.util.expression.ELUtil;
 
-public class LoginAuthorizationFilter  implements Filter {
-	public static final String ATTRIBUTE_LOGGED_ON_USER = "com.idega.block.login.LoggedOnUser";
-	public static final String USER_AUTHORIZATION_HEADER = RequestUtil.HEADER_AUTHORIZATION;
+public class LoginAuthorizationFilter implements Filter {
+
+	private static final Logger LOGGER = Logger.getLogger(LoginAuthorizationFilter.class.getName());
+
+	public static final String	ATTRIBUTE_LOGGED_ON_USER = "com.idega.block.login.LoggedOnUser",
+								USER_AUTHORIZATION_HEADER = RequestUtil.HEADER_AUTHORIZATION;
 
 	@Override
 	public void destroy() {
 	}
 
-	private Logger getLogger(){
-		return Logger.getLogger(LoginAuthorizationFilter.class.getName());
-	}
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse response,
-			FilterChain filterChain) throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		String authorization = request.getHeader(USER_AUTHORIZATION_HEADER);
+		if (StringUtil.isEmpty(authorization)) {
+			filterChain.doFilter(req, response);
+			return;
+		}
+
 		try {
 			LoginDAO loginDAO = ELUtil.getInstance().getBean(LoginDAO.BEAN_NAME);
-			HttpServletRequest request = (HttpServletRequest) req;
-			String authorization = request.getHeader(USER_AUTHORIZATION_HEADER);
-			User user = null;
-			if (!StringUtil.isEmpty(authorization)) {
-				user = loginDAO.authorize(authorization,
-						User.class);
+			User user = loginDAO.authorize(authorization, User.class);
+			if (user != null) {
+				request.setAttribute(ATTRIBUTE_LOGGED_ON_USER, user);
+				LoginRequestBean loginRequestBean = ELUtil.getInstance().getBean(LoginRequestBean.BEAN_NAME);
+				loginRequestBean.setAuthorizedUser(user);
+				loginRequestBean.setAuthorizedByFilter(true);
 			}
-			request.setAttribute(ATTRIBUTE_LOGGED_ON_USER, user);
-			LoginRequestBean loginRequestBean = ELUtil.getInstance().getBean(LoginRequestBean.BEAN_NAME);
-			loginRequestBean.setAuthorizedUser(user);
-			loginRequestBean.setAuthorizedByFilter(true);
 		} catch (Exception e) {
-			getLogger().log(Level.WARNING, "Failed authorizing user", e);
+			LOGGER.log(Level.WARNING, "Failed authorizing user", e);
 		}
 		filterChain.doFilter(req, response);
 	}
-	
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		// We can initialize a filter using the init-params here
 		// (which we defined in the deployment descriptor - web.xml)
 	}
+
 }
