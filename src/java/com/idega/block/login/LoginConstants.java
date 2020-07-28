02a22dbdc6639c1c1d21e89602b3eaa2bbd8e857
Valdas Žemaitis <valdas@idega.com>
2020-07-28 15:13:17 +0300

    New constants

--- src/java/com/idega/block/login/LoginConstants.java
+++ src/java/com/idega/block/login/LoginConstants.java
@@ -4,7 +4,10 @@ public class LoginConstants {
 
 	public static final String	IW_BUNDLE_IDENTIFIER = "com.idega.block.login",
 								TICKET_WEBSERVICE_PATH = "/TicketServices/Authentication",
-								LOGIN_TYPE = "login_type";
+								LOGIN_TYPE = "login_type",
+
+								OAUTH_DEFAULT_CLIENT_ID = "oauth.default_client_id",
+								OAUTH_UNEXPIRING_CLIENT_ID = "oauth.unexpiring_client_id";
 
 	public enum LoginType {
 
