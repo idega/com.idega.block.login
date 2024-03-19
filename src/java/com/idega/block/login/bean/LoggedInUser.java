e2b1591f113d9e861d581993a6b1fef05ceaed18
Valdas Žemaitis <valdas@idega.com>
2024-03-19 19:08:08 +0200

    Removed annotations

--- src/java/com/idega/block/login/bean/LoggedInUser.java
+++ src/java/com/idega/block/login/bean/LoggedInUser.java
@@ -2,12 +2,6 @@ package com.idega.block.login.bean;
 
 import java.io.Serializable;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 public class LoggedInUser implements Serializable {
 
 	private static final long serialVersionUID = -600623971979400484L;
